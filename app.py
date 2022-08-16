b5f79956981e1649d34450002af83f77c45e5eaf
uagard <102105537+uagard@users.noreply.github.com>
2022-08-16 18:09:37 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,22 +5,13 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/formula_one_analysis"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/2022_Formula_1_Analysis"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-   formula1 = mongo.db.formula.find_one()
-   return render_template("index.html", formula1=formula1)
-
-@app.route("/")
-def scrape():
-   formula1 = mongo.db.formula1
-   formula1_data = scraping.scrape_all()
-   formula1.update_one({}, {"$set":formula1_data}, upsert=True)
-   return redirect('/', code=302)
-
-.update_one(query_parameter, {"$set": data}, options)
+   RaceResults = mongo.db.RaceResults.find_one()
+   return render_template("index.html", RaceResults=RaceResults)
 
 if __name__ == "__main__":
-   app.run()
+   app.run()  
