00523f761d41dbeb444acf385082667d2a0dbed6
uagard <102105537+uagard@users.noreply.github.com>
2022-08-24 17:32:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template, redirect, url_for
+# Add dependencies
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scraping
+import cleaning
 
+# Init flask app
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/f1_app"
+# Use flask pymongo to set up mongo connection
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/f1_app'
 mongo = PyMongo(app)
 
-@app.route("/")
+# App routes
+@app.route('/')
 def index():
-   info = mongo.db.info.find_one()
-   return render_template("index.html", info=info)
+  f1 = mongo.db.f1.find_one()
+  return render_template('index.html', f1=f1)
 
+@app.route('/scrape')
+def scrapeAndClean():
+  f1 = mongo.db.f1
+  f1_data = cleaning.cleanAll()
+  # f1.update_one({}, {'$set':f1_data_json}, upsert=True)
+  # f1.insert_many(f1_data.to_dict('records'))
+  f1.update_one({}, {"$set":{'race':f1_data.to_html()}}, upsert=True)
 
-@app.route("/scrape")
-def scrape():
-   info = mongo.db.info
-   info_data = scraping.scrape_all()
-   info.update_one({}, {"$set":info_data}, upsert=True)
-   return redirect('/', code=302)
-
+  return redirect('/', code=302)
+   
 if __name__ == "__main__":
    app.run()
 
